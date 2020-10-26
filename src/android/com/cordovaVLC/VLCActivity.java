766f301f662967b2b558d1dcbef52374eb38c72a
embalint <embalint8@gmail.com>
2020-10-26 13:25:34 +0100

    android: hide nav bar

--- src/android/com/cordovaVLC/VLCActivity.java
+++ src/android/com/cordovaVLC/VLCActivity.java
@@ -945,11 +945,19 @@ public class VLCActivity extends Activity implements VlcListener, View.OnClickLi
         int width = 0;
 
         if(orientation.equals(PORTRAIT)) {
+            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             height = (int) (getDisplayMetrics().widthPixels/ratio);
              width = getDisplayMetrics().widthPixels;
             cameraViewParams.height = height;
             cameraViewParams.width = width;
         } else if(orientation.equals(LANDSCAPE)) {
+            activity.getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+            | View.SYSTEM_UI_FLAG_IMMERSIVE);
             height = (int) getDisplayMetrics().heightPixels;
             width = (int) (getDisplayMetrics().heightPixels * ratio);
             cameraViewParams.height = height;
