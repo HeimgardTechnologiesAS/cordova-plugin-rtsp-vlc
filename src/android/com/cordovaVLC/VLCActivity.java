205142bfd57f647b685ef67dfa73c423fd6386ac
Filip Strelec <filip.strelec@gmail.com>
2022-04-29 11:48:39 +0200

    Fix vlc offset

--- src/android/com/cordovaVLC/VLCActivity.java
+++ src/android/com/cordovaVLC/VLCActivity.java
@@ -224,6 +224,8 @@ public class VLCActivity extends Activity implements VlcListener, View.OnClickLi
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+
        
 
         activity = this;
